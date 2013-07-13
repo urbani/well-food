2052aa068ee0232227a341d9d70d7388fe1c07d3
Vatslav <vatslav.dovnar@yandex.ru>
2013-07-13 21:13:08 +0400

    merge commit

---
+++ trpo/src/Controller/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trpo.src.Controller
+{
+    class LoginController
+    {
+
+        LoginController()
+        {
+
+        }
+    }
+}
